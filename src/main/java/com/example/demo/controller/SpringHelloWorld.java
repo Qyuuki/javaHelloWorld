b5e7c36ac945c5b3ca486a133bc08400510accf5
ooshima yuuki <toradosioosima@gmail.com>
2022-07-01 12:13:21 +0900

    第6回課題作成:リクエストのクエリ文字列に応じてレスポンスを返す

--- src/main/java/com/example/demo/controller/SpringHelloWorld.java
+++ src/main/java/com/example/demo/controller/SpringHelloWorld.java
@@ -1,27 +1,26 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpringHelloWorld {
-	@GetMapping(value = "japan")
-	String japan() {
-		return "日本へようこそ！！";
-	}
 
-	@GetMapping(value = "us")
-	String us() {
-		return "Welcome to America！！";
-	}
+	@GetMapping(value = "greeting")
+	public String greeting(@RequestParam(value = "country", defaultValue = "japan") String country) {
+		switch (country) {
+		case "japan":
+			return "日本へようこそ！！";
+		case "us":
+			return "Welcome to America！！";
+		case "france":
+			return "Bienvenue en France！！";
+		case "korea":
+			return "한국에 오신 것을 환영합니다! !";
+		}
 
-	@GetMapping(value = "france")
-	String france() {
-		return "Bienvenue en France！！";
-	}
-
-	@GetMapping(value = "korea")
-	String korea() {
-		return "한국에 오신 것을 환영합니다! !";
+		return "該当する国はありません、japan, us, france, koreaのいずれかを指定して下さい。"
+				+ "\n例 : http://localhost:8080/greeting?country=japan";
 	}
 }
